eb92370bb36c2468d3cdd9efe277dda5778c36e8
gabrielivan <40808771+gabrielivan@users.noreply.github.com>
2019-05-12 23:16:43 -0300

    Add files via upload

---
+++ TP1_Recuperatorio/Entidades/Calculadora.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public static class Calculadora
+    {
+        /// <summary>
+        /// Metodo Validar Operador, se usa para que el operador que ingresa el usuario sea valido.
+        /// Si no es valido retorna +.
+        /// </summary>
+        /// <param name="operador">Es el operador a validar.</param>
+        /// <returns>Operador ya validado.</returns>
+
+        private static string ValidarOperador(string operador)
+        {
+            string resultado = "";
+            if (operador == "*" || operador == "/" || operador == "+" || operador == "-")
+            {
+                resultado = operador;
+            }
+            else
+            {
+                resultado = "+";
+            }
+            return resultado;
+        }
+
+        /// <summary>
+        /// Metodo Operar, se usa para realizar calculos.
+        /// </summary>
+        /// <param name="num1">Numero que se recibe para operar.</param>
+        /// <param name="num2">Numero que se recibe para operar.</param>
+        /// <param name="operador">Operador a utilizar.</param>
+        /// <returns>Resultado de la operacion solicitada.</returns>
+
+        public static double Operar(Numero num1, Numero num2, string operador)
+        {
+            string operadorValidado = ValidarOperador(operador);
+            double resultado = 0;
+
+            switch (operadorValidado)
+            {
+                case "+":
+                    {
+                        resultado = num1 + num2;
+                        break;
+                    }
+                case "-":
+                    {
+                        resultado = num1 - num2;
+                        break;
+                    }
+                case "*":
+                    {
+                        resultado = num1 * num2;
+                        break;
+                    }
+                case "/":
+                    {
+                        resultado = num1 / num2;
+                        break;
+                    }
+            }
+            return resultado;
+        }
+    }
+}
---
+++ TP1_Recuperatorio/MiCalculadora/FormCalculadora.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+
+namespace MiCalculadora
+{
+    public partial class FormCalculadora : Form
+    {
+        public FormCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Boton: "Operar", Opera con el metodo Operar de la clase MiCalculadora
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            double resultado = 0;
+
+            resultado = FormCalculadora.Operar(txtNumero1.Text, txtNumero2.Text, cmbOperador.Text);
+            lblResultado.Text = Convert.ToString(resultado);
+
+        }
+
+        /// <summary>
+        /// Boton: "Limpiar", Limpia la calculadora, vaciando los campos.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtNumero1.Text = "";
+            txtNumero2.Text = "";
+            cmbOperador.Text = "";
+            lblResultado.Text = "";
+        }
+
+        /// <summary>
+        /// Boton: "Cerrar", Cierra la calculadora.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// Boton: "Convertir a Binario", Calcula la operacion llamando al metodo DecimalBinario de la clase Numero.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+        private void btnConvertirABinario_Click(object sender, EventArgs e)
+        {
+            Numero numero = new Numero();
+            if (lblResultado.Text != "")
+            {
+                lblResultado.Text = numero.DecimalBinario(lblResultado.Text);
+            }
+        }
+
+        /// <summary>
+        /// Boton: "Convertir a Decimal", Calcula la operacion llamando al metodo BinarioDecimal de la clase Numero.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+        private void btnConvertirADecimal_Click(object sender, EventArgs e)
+        {
+            Numero numero = new Numero();
+            if (lblResultado.Text != "")
+            {
+                lblResultado.Text = numero.BinarioDecimal(lblResultado.Text);
+            }
+        }
+
+        /// <summary>
+        /// Calcula la operacion llamando al metodo Operar de la clase Calculadora
+        /// </summary>
+        /// <param name="numero1">Parametro de tipo string que se le envia al metodo Operar de la clase Calculadora</param>
+        /// <param name="numero2">Parametro de tipo string que se le envia al metodo Operar de la clase Calculadora</param>
+        /// <param name="operador">Parametro de tipo string que se le envia al metodo Operar de la clase Calculadora</param>
+        /// <returns></returns>
+        private static double Operar(string numero1, string numero2, string operador)
+        {
+            Numero numeroUno = new Numero(numero1);
+            Numero numeroDos = new Numero(numero2);
+            double resultado = Calculadora.Operar(numeroUno, numeroDos, operador);
+            return resultado;
+        }
+    }
+}
